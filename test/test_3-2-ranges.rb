0ff99354eac3e03f8d0d10255c183a19c53a534c
Martin Vysny <mavi@vaadin.com>
2025-10-14 14:47:53 +0300

    tests for 3-2

---
+++ test/test_3-2-ranges.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require_relative '../3-2-ranges.rb'
+
+class MyDateRangeTest < Minitest::Test
+  def test_include
+    r = (MyDate.new(2024, 1, 1)..MyDate.new(2024, 1, 10))
+    assert !(r.include? MyDate.new(2025, 1, 5))
+    assert(r.include? MyDate.new(2024, 1, 5))
+  end
+end
+
