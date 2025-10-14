fea08de82f700fded3656d8cca3cff51cf2ecd6b
Martin Vysny <mavi@vaadin.com>
2025-10-14 13:12:04 +0300

    Tests for 2-1

---
+++ test/test_2-1-data-classes.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require_relative '../2-1-data-classes.rb'
+
+class TestPeople < Minitest::Test
+  def test_get_people
+    assert_equal "Alice:29, Bob:31", get_people().map { "#{it.name}:#{it.age}" } .join(", ")
+  end
+  def test_compare_people
+    assert compare_people()
+  end
+end
+
