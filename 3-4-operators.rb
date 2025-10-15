7e075469901ced6e0ff5f4b24926d3aee6ed6959
Martin Vysny <mavi@vaadin.com>
2025-10-15 12:02:26 +0300

    Add support for YARD

--- 3-4-operators.rb
+++ 3-4-operators.rb
@@ -1,11 +1,25 @@
 require_relative '3-1-comparison.rb'
 
-##
-# A simple duration implementation of given number of +years+ and +days+ (both +Numeric+).
+# A simple duration implementation of given number of `years` and `days` (both Numeric).
 class MyDuration < Data.define(:years, :days)
   DAY = MyDuration.new(0, 1)
   WEEK = MyDuration.new(0, 7)
   YEAR = MyDuration.new(1, 0)
+
+  # Initializes new MyDuration.
+  #
+  # @param years [Numeric] the number of years
+  # @param days [Numeric] the number of days
+  # @raise [ArgumentError] if years or days is not Numeric
+  def initialize(map)
+    super
+    raise ArgumentError, "years must be numeric: #{years}" unless years.is_a? Numeric
+    raise ArgumentError, "days must be numeric: #{days}" unless days.is_a? Numeric
+  end
+
+  # Creates MyDuration out of a symbol
+  #
+  # @param symbol [Symbol] one of `:day`, `:week`, `:year`
   def self.of(symbol)
     return MyDuration::DAY if symbol == :day
     return MyDuration::WEEK if symbol == :week
@@ -21,16 +35,16 @@ class MyDuration < Data.define(:years, :days)
 end
 
 class MyDate
-  ##
   # Returns a new date x days in the future (or past if x is negative)
-  # +days+:: how many days to add, +Numeric+. May be negative.
+  #
+  # @param days [Numeric] how many days to add. May be negative.
   def plus_days(days)
     date = to_date + days
     return MyDate.new(date.year, date.month, date.day)
   end
-  ##
   # Returns a new date x days in the future (or past if x is negative)
-  # +other+:: If +Numeric+, interpreted as number of days. May also be +MyDuration+ or one of the following symbols: :day, :week, :year
+  #
+  # @param other [Numeric, MyDuration, Symbol] If Numeric, interpreted as number of days. May also be MyDuration or one of the following symbols: `:day`, `:week`, `:year`
   def +(other)
     other = MyDuration.of(other) if other.is_a? Symbol
     return plus_days(other) if other.is_a? Numeric
--- Rakefile
+++ Rakefile
@@ -1,7 +1,19 @@
 require 'rake/testtask'
+require 'yard'
 
 Rake::TestTask.new do |t|
   t.pattern = 'test/test_*.rb' # Run all files in test/ starting with test_
 #  t.verbose = true              # Show detailed test output
+end
+
+# Define a YARD task for generating documentation
+YARD::Rake::YardocTask.new do |t|
+  t.files = ['*.rb'] # Files to include (all Ruby files in lib/)
+  t.options = [
+    '--title', 'Kotlin Koans in Ruby', # Title for the generated docs
+    '--main', 'README.md', # Main documentation file
+    '--output-dir', 'doc', # Output directory
+    '--markup', 'markdown' # Use Markdown for processing
+  ]
 end
 
