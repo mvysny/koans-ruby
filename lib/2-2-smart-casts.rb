914a7e193d5a8ed84fec133603e161be965ef776
Martin Vysny <mavi@vaadin.com>
2025-10-15 17:39:01 +0300

    rdoc

--- lib/2-2-smart-casts.rb
+++ lib/2-2-smart-casts.rb
@@ -1,10 +1,14 @@
+# A numeric expression, which simply causes {#calculate} to return `value`.
 class Num < Data.define(:value)
+  # Returns `value`
   def calculate
     value
   end
 end
 
+# Calculates sum of two expressions, `left` and `right`.
 class Sum < Data.define(:left, :right)
+  # Returns the sum of `left` and `right`
   def calculate
     left.calculate + right.calculate
   end
