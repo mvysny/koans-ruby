b12cfe15776835ead6d6770bd706573305fb7c3b
Martin Vysny <mavi@vaadin.com>
2025-10-23 10:01:23 +0300

    refactoring

--- lib/4-2-sort.rb
+++ lib/4-2-sort.rb
@@ -1,10 +1,9 @@
-require '4-1-shop.rb'
+require '4-1-shop'
 
 class Shop
   # Returns a list of customers, sorted in the descending by number of orders they have made
   # @return [Array<Customer>]
-  def get_customers_sorted_by_order
+  def customers_sorted_by_order
     customers.sort_by { -it.orders.size }
   end
 end
-
--- test/test_4-2-sort.rb
+++ test/test_4-2-sort.rb
@@ -1,10 +1,9 @@
 require 'minitest/autorun'
-require '4-2-sort.rb'
+require '4-2-sort'
 
 class TestSort < Minitest::Test
-  def test_get_customers_sorted_by_order
-    assert_equal "Jouni, Matti", SHOPS[0].get_customers_sorted_by_order.map { it.name } .join(', ')
-    assert_equal "Matti, Jouni", SHOPS[1].get_customers_sorted_by_order.map { it.name } .join(', ')
+  def test_customers_sorted_by_order
+    assert_equal 'Jouni, Matti', SHOPS[0].customers_sorted_by_order.map { it.name }.join(', ')
+    assert_equal 'Matti, Jouni', SHOPS[1].customers_sorted_by_order.map { it.name }.join(', ')
   end
 end
-
