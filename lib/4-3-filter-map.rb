0a3f5bb4c6e4fe16b704384cda54705ceed62635
Martin Vysny <mavi@vaadin.com>
2025-10-23 10:50:29 +0300

    formatting

--- lib/4-3-filter-map.rb
+++ lib/4-3-filter-map.rb
@@ -1,18 +1,18 @@
-require '4-1-shop.rb'
+require '4-1-shop'
 
 class Shop
   # Find all the different cities the customers are from
   # @return [Array<City>]
   def get_customer_cities
-    customers.map { it.city } .uniq
+    customers.map { it.city }.uniq
   end
-  
+
   # Find the customers living in a given city
   # @param city [City]
   # @return [Array<Customer>]
   def get_customers_from(city)
     raise ArgumentError unless city.is_a? City
+
     customers.filter { it.city == city }
   end
 end
-
