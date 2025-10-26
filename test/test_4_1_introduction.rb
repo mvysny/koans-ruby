755a3a050897f41719c68f7a6b354843a553bbd6
Martin Vysny <mavi@vaadin.com>
2025-10-26 21:39:29 +0200

    renaming

---
+++ test/test_4_1_introduction.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require '4_1_introduction'
+
+class TestIntroduction < Minitest::Test
+  def test_get_set_of_customers
+    assert_equal 'Matti, Jouni', SHOPS[0].get_set_of_customers.map { it.name }.join(', ')
+    assert_equal 'Matti, Jouni', SHOPS[1].get_set_of_customers.map { it.name }.join(', ')
+  end
+end
