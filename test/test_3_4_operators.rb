875098e3d38f8d269a164030ed1c8a0299654215
Martin Vysny <mavi@vaadin.com>
2025-10-26 21:38:13 +0200

    renaming

--- test/test_3_4_operators.rb
+++ test/test_3_4_operators.rb
@@ -1,21 +1,25 @@
 require 'minitest/autorun'
-require '3-4-operators.rb'
+require '3_4_operators'
 
 class TestOperators < Minitest::Test
   def test_plus_days
     assert_equal MyDate.new(2024, 1, 2), (MyDate.new(2024, 1, 1) + 1)
   end
+
   def test_plus_day
     assert_equal MyDate.new(2024, 1, 2), (MyDate.new(2024, 1, 1) + :day)
   end
+
   def test_plus_week
     assert_equal MyDate.new(2024, 1, 8), (MyDate.new(2024, 1, 1) + :week)
   end
+
   def test_plus_year_week
     assert_equal MyDate.new(2025, 1, 8), (MyDate.new(2024, 1, 1) + :year + :week)
   end
+
   def test_plus_duration
-    assert_equal MyDate.new(2026, 1, 27), (MyDate.new(2024, 1, 1) + MyDuration::YEAR * 2 + MyDuration::WEEK * 3 + MyDuration::DAY * 5)
+    assert_equal MyDate.new(2026, 1, 27),
+                 (MyDate.new(2024, 1, 1) + MyDuration::YEAR * 2 + MyDuration::WEEK * 3 + MyDuration::DAY * 5)
   end
 end
-
