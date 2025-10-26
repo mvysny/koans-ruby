c06cd4630a2a3d395f40fe080b591427e1f02ed9
Martin Vysny <mavi@vaadin.com>
2025-10-26 15:22:22 +0200

    4_4

--- lib/4_4_all_any.rb
+++ lib/4_4_all_any.rb
@@ -1,9 +1,16 @@
 require '4_1_shop'
 
 class Shop
-  # Checks whether all customers are from given city
+  # Checks whether all customers are from given `city`
   # @param city [City]
+  # @return [Boolean]
   def all_customers_from?(city)
     customers.all? { it.city == city }
+  end
+
+  # Return true if there is at least one customer from a given `city`
+  # @param city [City]
+  def has_customer_from?(city)
+    customers.any? { it.city == city }
   end
 end
--- test/test_4_4_all_any.rb
+++ test/test_4_4_all_any.rb
@@ -6,4 +6,11 @@ class TestAllAny < Minitest::Test
     assert !KMARKET.all_customers_from?(TURKU)
     assert !KMARKET.all_customers_from?(HELSINKI)
   end
+
+  def test_has_customer_from
+    assert KMARKET.has_customer_from?(TURKU)
+    assert SMARKET.has_customer_from?(TURKU)
+    assert KMARKET.has_customer_from?(HELSINKI)
+    assert SMARKET.has_customer_from?(HELSINKI)
+  end
 end
