2a8823acf2954e78a40df03fa7ce7c9fc341f129
Martin Vysny <mavi@vaadin.com>
2025-10-26 18:16:13 +0200

    minor

--- lib/1-8-lambdas.rb
+++ lib/1-8-lambdas.rb
@@ -1,5 +1,6 @@
+# Returns true if given collection contains even numbers
+# @param collection [Array<Integer>] numbers to check
+# @return [Boolean] true if any of given numbers is even
 def contains_even(collection)
-  collection.any? { it.even? }
+  collection.any?(&:even?)
 end
-
-
