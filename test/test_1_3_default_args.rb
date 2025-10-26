171ca067f4dee939c7eb98d2354c2c40d672508c
Martin Vysny <mavi@vaadin.com>
2025-10-26 18:27:39 +0200

    fix file naming scheme

--- test/test_1_3_default_args.rb
+++ test/test_1_3_default_args.rb
@@ -1,9 +1,8 @@
 require 'minitest/autorun'
-require '1-3-default-args.rb'
+require '1_3_default_args'
 
 class TestFoo < Minitest::Test
   def test_use_foo
-    assert_equal ["a42", "b1", "C42", "D2"], use_foo()
+    assert_equal %w[a42 b1 C42 D2], use_foo
   end
 end
-
