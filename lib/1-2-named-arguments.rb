ae50b577c9a6e721110c834d99ec1f9d7b16912b
Martin Vysny <mavi@vaadin.com>
2025-10-26 15:04:46 +0200

    rdoc

--- lib/1-2-named-arguments.rb
+++ lib/1-2-named-arguments.rb
@@ -1,4 +1,6 @@
+# Joins an array of objects to a comma-separated string
+# @param strings [Array<#to_s>] objects that respond to `#to_s`.
+# @return [String] a comma-separated string
 def join_options(strings)
   '[' + strings.join(', ') + ']'
 end
-
