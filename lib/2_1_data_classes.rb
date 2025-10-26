e24317667f3784fb2b26884424bd2fe85749ce84
Martin Vysny <mavi@vaadin.com>
2025-10-26 21:16:37 +0200

    refactoring

---
+++ lib/2_1_data_classes.rb
@@ -0,0 +1,16 @@
+# A person with a {String} `name` and {Integer} `age`
+class Person < Data.define(:name, :age)
+end
+
+# @return [Array<Person>] a demo set of people
+def get_people
+  [Person.new("Alice", 29), Person.new("Bob", 31)]
+end
+
+# @return [Boolean] true if {Person} comparison works properly.
+def compare_people
+  p1 = Person.new("Alice", 29)
+  p2 = Person.new("Alice", 29)
+  p1 == p2
+end
+
--- test/test_2_1_data_classes.rb
+++ test/test_2_1_data_classes.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require '2-1-data-classes.rb'
+require '2_1_data_classes'
 
 class TestPeople < Minitest::Test
   def test_get_people
