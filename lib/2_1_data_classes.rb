35c38860811c0d79416c6558aa565bb348fd3fe7
Martin Vysny <mavi@vaadin.com>
2025-10-26 21:21:36 +0200

    minor

--- lib/2_1_data_classes.rb
+++ lib/2_1_data_classes.rb
@@ -4,13 +4,12 @@ end
 
 # @return [Array<Person>] a demo set of people
 def get_people
-  [Person.new("Alice", 29), Person.new("Bob", 31)]
+  [Person.new('Alice', 29), Person.new('Bob', 31)]
 end
 
 # @return [Boolean] true if {Person} comparison works properly.
 def compare_people
-  p1 = Person.new("Alice", 29)
-  p2 = Person.new("Alice", 29)
+  p1 = Person.new('Alice', 29)
+  p2 = Person.new('Alice', 29)
   p1 == p2
 end
-
--- test/test_2_1_data_classes.rb
+++ test/test_2_1_data_classes.rb
@@ -3,10 +3,10 @@ require '2_1_data_classes'
 
 class TestPeople < Minitest::Test
   def test_get_people
-    assert_equal "Alice:29, Bob:31", get_people().map { "#{it.name}:#{it.age}" } .join(", ")
+    assert_equal 'Alice:29, Bob:31', get_people.map { "#{it.name}:#{it.age}" }.join(', ')
   end
+
   def test_compare_people
-    assert compare_people()
+    assert compare_people
   end
 end
-
