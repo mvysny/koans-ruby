2367af171a9892556ac315743f51f3a2a4c4101a
Martin Vysny <mavi@vaadin.com>
2025-10-26 15:09:34 +0200

    rdoc

--- lib/1-3-default-args.rb
+++ lib/1-3-default-args.rb
@@ -1,11 +1,17 @@
+# A foo function
+# @param name [String]
+# @param number [Integer]
+# @param to_upper_case [Boolean]
+# @return [String] stuff
 def foo(name, number: 42, to_upper_case: false)
   (to_upper_case ? name.upcase : name) + number.to_s
 end
 
-def use_foo()
-  [foo("a"), 
-   foo("b", number: 1), 
-   foo("c", to_upper_case: true),
-   foo("d", number: 2, to_upper_case: true)]
+# Calls {#foo} and returns an array of stuff
+# @return [Array<String>] stuff
+def use_foo
+  [foo('a'),
+   foo('b', number: 1),
+   foo('c', to_upper_case: true),
+   foo('d', number: 2, to_upper_case: true)]
 end
-       
