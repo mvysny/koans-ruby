7a2cf05c98008f0ee62cc393540de92c655cf35e
Martin Vysny <mavi@vaadin.com>
2025-10-26 21:23:44 +0200

    refactoring

--- test/test_2_2_smart_casts.rb
+++ test/test_2_2_smart_casts.rb
@@ -1,12 +1,12 @@
 require 'minitest/autorun'
-require '2-2-smart-casts.rb'
+require '2_2_smart_casts'
 
 class TestNumSum < Minitest::Test
   def test_num_calculate
     assert_equal 5, Num.new(5).calculate
   end
+
   def test_sum_calculate
     assert_equal 8, Sum.new(Num.new(5), Num.new(3)).calculate
   end
 end
-
