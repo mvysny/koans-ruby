500281219c242e2278a106187c9f017c98b7fc32
Martin Vysny <mavi@vaadin.com>
2025-10-27 09:33:28 +0200

    4-5

--- lib/4_5_associate.rb
+++ lib/4_5_associate.rb
@@ -6,4 +6,21 @@ class Shop
   def name_to_customer_map
     customers.map { [it.name, it] } .to_h
   end
+
+  # Build a map from the customer to their city
+  # @return [Hash{Customer => City}]
+  def customer_to_city_map
+    customers.map { [it, it.city] } .to_h
+  end
+
+  # @return [Customer | nil]
+  def customer_by_name(name)
+    customers.find { it.name == name }
+  end
+
+  # Build a map from the customer name to their city
+  # @return [Hash{String => City}]
+  def customer_name_to_city_map
+    customer_to_city_map.transform_keys { it.name }
+  end
 end
--- test/test_4_5_associate.rb
+++ test/test_4_5_associate.rb
@@ -6,4 +6,12 @@ class TestAssociate < Minitest::Test
     assert_equal 'Jouni', SMARKET.name_to_customer_map['Jouni']&.name
     assert_nil KMARKET.name_to_customer_map['Carl']&.name
   end
+
+  def test_customer_to_city_map
+    assert_equal HELSINKI, SMARKET.customer_to_city_map[SMARKET.customer_by_name('Jouni')]
+  end
+
+  def test_customer_name_to_city_map
+    assert_equal HELSINKI, SMARKET.customer_name_to_city_map['Jouni']
+  end
 end
