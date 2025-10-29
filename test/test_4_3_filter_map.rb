7c68425022c29c34a00bd3840103ea060fe6c716
Martin Vysny <mavi@vaadin.com>
2025-10-29 20:47:23 +0200

    minor

--- test/test_4_3_filter_map.rb
+++ test/test_4_3_filter_map.rb
@@ -5,14 +5,14 @@ require '4_3_filter_map'
 
 class TestFilterMap < Minitest::Test
   def test_customer_cities
-    assert_equal 'Turku, Helsinki', SHOPS[0].customer_cities.map(&:name).join(', ')
-    assert_equal 'Turku, Helsinki', SHOPS[1].customer_cities.map(&:name).join(', ')
+    assert_equal %w[Turku Helsinki], SHOPS[0].customer_cities.map(&:name)
+    assert_equal %w[Turku Helsinki], SHOPS[1].customer_cities.map(&:name)
   end
 
   def test_get_customers_from
-    assert_equal 'Jouni', SHOPS[0].get_customers_from(HELSINKI).map(&:name).join(', ')
-    assert_equal 'Matti', SHOPS[0].get_customers_from(TURKU).map(&:name).join(', ')
-    assert_equal 'Jouni', SHOPS[1].get_customers_from(HELSINKI).map(&:name).join(', ')
-    assert_equal 'Matti', SHOPS[1].get_customers_from(TURKU).map(&:name).join(', ')
+    assert_equal ['Jouni'], SHOPS[0].get_customers_from(HELSINKI).map(&:name)
+    assert_equal ['Matti'], SHOPS[0].get_customers_from(TURKU).map(&:name)
+    assert_equal ['Jouni'], SHOPS[1].get_customers_from(HELSINKI).map(&:name)
+    assert_equal ['Matti'], SHOPS[1].get_customers_from(TURKU).map(&:name)
   end
 end
