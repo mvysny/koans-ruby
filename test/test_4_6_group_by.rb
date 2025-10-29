02ebd5d573e22c35a5bf36d42092396ff2086866
Martin Vysny <mavi@vaadin.com>
2025-10-29 20:54:04 +0200

    minor

--- test/test_4_6_group_by.rb
+++ test/test_4_6_group_by.rb
@@ -6,7 +6,7 @@ require '4_6_group_by'
 class TestGroupBy < Minitest::Test
   def test_group_customers_by_city
     a = KMARKET.group_customers_by_city
-    assert_equal(['Jouni'], a[HELSINKI].map { it.name })
-    assert_equal(['Matti'], a[TURKU].map { it.name })
+    assert_equal(['Jouni'], a[HELSINKI].map(&:name))
+    assert_equal(['Matti'], a[TURKU].map(&:name))
   end
 end
